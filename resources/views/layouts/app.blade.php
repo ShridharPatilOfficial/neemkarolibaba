bf27860ba0d112c0c2ac8c0eb2a5a1563a59417d
Shridhar Patil <shridharpatil826@gmail.com>
2026-04-14 02:08:23 +0530

    SEO Verification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="xGChHEAZUfdePsZtCZ25SZmYZZb1ySZkLlJ-l93ecpg" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @php
         $metaTitle = trim(strip_tags(\Illuminate\Support\Str::limit(
